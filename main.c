2cc51083cd56f2e3e9f20a11865024a73a100972
Joel-ismael <joelismaeldomguiatakam@gmail.com>
2025-09-24 16:31:55 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+  char nombrestr[50];
+
+  // demande a l'utilisateur d'entree la valeur
+printf(" Entre la chain de charactere en decimal : \n");
+ fgets(nombrestr, sizeof(nombrestr),stdin);
+
+ // conversion
+ double nobre_reel=atof(nombrestr);
+
+ //Afficher le nombre
+ printf(" Le nombre reel est : %.2f\n", nobre_reel);
+
+ return 0;/* Communiqué :
+Toutes la chorale sans exception est attendu jeudi dès 20h à la veillée mortuaire du mari de l'ancienne de l'église maman Élise qui aura lieu ce jeudi à partir de 20 h au quartier Nkoabang en face de la RENAPV . Le lieu de départ sera à l'église. Merci 
+Excellente nuit à tous shalommm 🙏🙏♥️*/
+
+}
